4c38bc01b0bddc651c534ead2d75fdc38d9142c1
Paulo <pc5783346@gmail.com>
2022-07-26 19:46:48 -0300

    feat: retorna uma tabela de depoimentos com o nome dos remetentes, destinatários e as mensagens

--- pratica_join.sql
+++ pratica_join.sql
@@ -5,3 +5,15 @@ SELECT
 	cities.name AS city
 FROM users JOIN cities
 ON users."cityId" = cities.id;
+
+--QUESTÃO 2
+SELECT
+	testimonials.id,
+	u1.name AS writer,
+	u2.name AS recipient,
+	testimonials.message
+FROM testimonials
+JOIN users u1 ON
+testimonials."writerId"=u1.id
+JOIN users u2 ON
+testimonials."recipientId"=u2.id;
